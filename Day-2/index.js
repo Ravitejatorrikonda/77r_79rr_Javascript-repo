5a303d1383e42c1c5f4df45fc1246b257ade804b
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2026-02-03 17:06:46 +0530

    Variables and History

---
+++ Day-2/index.js
@@ -0,0 +1,83 @@
+//->single line comments
+/*multi line comments */
+/**
+ * documentations comments
+ * 
+ * Variables in Javascript
+ * 
+ * ---------------------------
+ * 
+ * variables are acts as a container, which is used to store the data(primitive/non-primitive).to make external operations
+ * 
+ * 
+ * 
+ * they are diff types of variables in JS:-
+ * 
+ * var
+ * let
+ * const
+ * automatic
+ * 
+ * sy:-
+ * 
+ * keyword identifier=value;
+ * Ex:-
+ * var variable_Name=value;
+ * 
+ * 
+ * 
+ * 
+ */
+
+// var a=34;//intilization
+// console.log(a)//34
+
+// var a;//diclarartion
+// console.log(a)//34, un,defa
+
+// b=43;//->assign
+// console.log(b)//error, 43
+
+// var _name="Charan"
+// console.log(_name)//charan
+
+// var Name="javascript"
+// console.log(name);//space
+// console.log(_name);//charan
+// // console.log(_Name);//space
+// console.log(Name);//javascript
+// console.log(name);
+
+// var 123=123;
+// console.log(123);
+
+// var &Abc=123;
+// console.log(&Abc);//syntax
+
+// var _Ab$cd="hello"
+// console.log(_Ab$Cd);//not def
+
+
+// var _Ab$ cd="hello"
+// console.log(_Ab$ cd);//
+
+// var let="Hello"
+// console.log(let);
+
+
+console.log(a)//a sy not  error a not
+var a=34;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
