c2401668c7665a1d3d065f948a700fd77a486a8d
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2026-02-04 16:14:14 +0530

    GEC

---
+++ Day-3/index.js
@@ -0,0 +1,114 @@
+/**
+ * 
+ * 
+ * TDZ
+ * 
+ * hoisting is process of accessing variables ,functions and class above its scope
+ * 
+ * Hoisting is a process of accessing the variables before intilization
+ * 
+ * BEC GEC
+ * 
+ * GEC:-
+ * -----
+ * GEC stands for Global exicution context
+ * 
+ * the starting and ending point of our javascript will take care by the GEC
+ * inside the function code will take care by the FEC
+ * 
+ * var a=10
+ * 
+ * function x(){
+ * console.log(x)
+ * 
+ *  in this is GEC there is two memory phases
+ * 
+ * 1 memory phase
+ * 2. exicution phase
+ * 
+ * NOTE:-
+ * -----
+ * at the time of memory phase variable var can be diclared as a undefined
+ * 
+ * ->windo object is created
+ * ->function will store as a function defination
+ * 
+ * 
+ */
+
+
+
+
+// console.log(a)
+// var a=10;
+
+// console.log(a);
+// console.log(b);
+// var a=100;
+// console.log(c);
+// var b=200;
+// console.log(b);
+// var c=a+10;
+// console.log(a);
+// console.log(c);
+
+// console.log(a,b,c,d);//un error 
+// var a=10;
+// console.log(a);//10
+// var b=20;
+// console.log(c);//un
+// var c=30;
+// console.log(b);//20
+// var d=40;
+// console.log(d+200);//240
+
+// var a;
+// var c;
+// console.log(a+c);
+
+/**
+ * Data Types in JS:-
+ * ------------------
+ * 
+ * primitive and Non primitive
+ * 
+ * primitive Data Types in JS:-
+ * ---------------------------------
+ * 
+ * pritive data types are immutable. we can not change the value of the varible
+ * 
+ * Number
+ * String
+ * boolean
+ * undefined
+ * null
+ * symbol
+ * 
+ * Non-primitive data types:-
+ * Non primitive data types are muttable.once assigning a value to the variable possible to change 
+ * 
+ * arrays
+ * objects
+ * func
+ * set
+ * map
+ * regexp
+ * 
+ * 
+ * 
+ * 
+ */
+
+var a=null;
+console.log(typeof(a))
+
+
+
+
+
+
+
+
+
+
+
