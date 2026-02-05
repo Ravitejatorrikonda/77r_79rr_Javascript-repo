e7a00621ca8f400c529cd26bc5abe751a9925a31
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2026-02-05 16:47:31 +0530

    Operatores

---
+++ Day-4/index.js
@@ -0,0 +1,76 @@
+/**
+ * 
+ * Operatores:-
+ * Operatores are a special symbols which is used to perform a special operation using values
+ * they are diff types of operatores:-
+ * 
+ * Airthamtic operatore-> + - * / %
+ * Assignment operatore  = += -= /=
+ * comparision operatore  == === <= >=
+ * logical operatore  && || !
+ * ternary operatore
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+// var biryani1=100
+// var biryani2=100
+// console.log(biryani1+biryani2)
+
+// var wallet=1000
+
+// var wallet=1000
+//   wallet -=400//
+//   console.log(wallet);
+
+//   var age=18;
+//   console.log(age==="18");
+
+// console.log(true+true);//true true 2
+// console.log(true+false);//1
+// console.log(false+true);//1
+// console.log(false+false);//0
+
+// let age=prompt("enter your age")
+// console.log(10-age)
+
+// Number()
+//boolean()
+//string()
+
+
+// var a=Boolean(0)
+// var b=1
+// console.log(a+b);//1 true false
+
+
+// var age1=+prompt("enter age")
+// var age2=+prompt("enter age")
+// // console.log(Number(age1)+Number(age2));
+// console.log(age1+age2);
+
+// var x=10;
+// console.log(++x);
+
+/**
+ * 
+ * 
+ * condition?expression1:expression2;
+ * 
+ */
+
+
+// var age=prompt("enter age")
+// var result=age>=18?"majore":"teen";
+// console.log(result);
+
+
+
+
+
+  
+  
+
