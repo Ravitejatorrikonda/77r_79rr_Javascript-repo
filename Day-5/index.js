0868c841476162c5a791e26cf0c7c536df813253
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2026-02-09 17:20:53 +0530

    conditional statement

---
+++ Day-5/index.js
@@ -0,0 +1,95 @@
+/**
+ * if condition
+ * if else
+ * else if lader 
+ * switch
+ * 
+ * 
+ */
+// var age=20;
+// if(age>=18){
+//    console.log("can vote");
+// }
+// let age=9;
+// if(age<=12){
+//   console.log("child ticket");
+// }
+// else if(age>12 && age<=55){
+//   console.log("middle person ticket");
+// }
+// else{
+//  console.log("senior ticket");
+// }
+
+// var day=8;
+// switch(day){
+//     case 0:
+//         console.log("weekend");
+//         break;
+//     case 1:
+//         console.log("starting day");
+//         break;
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//         console.log("mid week");
+//         break;
+//     case 6:
+//         console.log("weekend");
+//         break;
+//     default:
+//         console.log("nothing");
+// }
+
+// var citizen="American";
+// if(citizen=="Indian")
+// {
+//     var gender="male";
+//     if(gender=="male")
+//     {
+//         console.log("Male Indian Citizen");
+//     }
+//     if(gender=="female")
+//     {
+//         console.log("Female Indian Citizen");
+//     }
+// }
+// else
+// {
+//     console.log("Not an Indian Citizen");
+    
+// }
+
+// debugger;
+// var laptop=true;
+
+// if(laptop){
+//     console.log("attended zoom class");
+    
+// }
+// else{
+//     console.log("not attended");
+    
+// }
+
+/**
+ * history of js
+ * what is js
+ * datatypes
+ * variables
+ * V8 engine
+ * hoisting'
+ * GEC
+ * operatores
+ * conditional statements
+ * 
+ * 
+ * 
+ */
+
+
+
+
+
+
