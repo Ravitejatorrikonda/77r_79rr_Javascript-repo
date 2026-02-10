86c203affd0c9d91c1a89ff7ed9fe6dc82ab82f3
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2026-02-10 17:59:58 +0530

    fun-1

---
+++ Day-6/index.js
@@ -0,0 +1,97 @@
+/**
+ * 
+ * functions in JS:-
+ * -------------------
+ * function is a block of code it can be used as a organize our javascript code. we can re use  any no off times and named functions can be hoisted
+ * 
+ * functions are re usable block of code
+ *  
+ * diff types of functions:-
+ * ->named fun
+ * ->first class fun
+ * ->anonymous fun
+ * ->arrow fun
+ * ->constructore fun
+ * ->call back fun
+ * ->higher order fun
+ * ->lower order fun
+ * ->pure fun
+ * ->impure fun
+ * ->dafault parameter fun
+ * ->optional parameter fun
+ * ->rest parameter fun
+ * ->immediate invoked fun
+ * ->generatore fun
+ * 
+ * 
+ * 
+ * sy:-
+ * ----
+ * 
+ * fun defination:-
+ * -------------------
+ *       function fun_Name([parameters...]){
+ *        //statements
+ *            [return]
+ *     }
+ * 
+ * Note:-  function defination is always stored in a function name
+ * fun calling:-
+ * -------------
+ * fun_Name();
+ * log(Fun_Name)
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+// function print(){
+//     console.log("Hi there");
+    
+// }
+
+// // console.log(print);
+// print();
+
+// function sum(){
+//     console.log(10+20)
+// }
+// console.log(sum);
+
+// function kitchen(){
+//     console.log("coocking started");
+//     order();
+//     order();
+//     console.log("coocking stoped");
+    
+    
+// }
+// function order1(){
+//     console.log("order taken");
+//     console.log("went to kitchen");
+    
+    
+// }
+// function order2(){
+//     console.log("order taken 2");
+//     console.log("went to kitchen 2");
+    
+    
+// }
+// kitchen();
+
+function print(){
+   
+    console.log("hello there");
+    
+}
+print();
+
+
+
+
+
+
