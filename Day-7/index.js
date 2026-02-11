85577accc62cd848c9132142133822be6ea62377
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2026-02-11 16:24:51 +0530

    fun-2

---
+++ Day-7/index.js
@@ -0,0 +1,108 @@
+/**
+ * 
+ * fun def:-
+ * 
+ * 
+ * function funName([parameters....]){
+ * //statements
+ * [return...]
+ * 
+ * }
+ * 
+ * funcationname();
+ * 
+ * 
+ */
+
+
+// function getMaggie(maggie){
+//     console.log(`2 packets of ${maggie} ready to open`);
+
+// }
+// getMaggie("yippe");
+
+
+// function biryani(per,rate){
+//     console.log(per*rate);
+
+// }
+// biryani(2,120)
+
+// function pushpa(hero, sidechar) {
+//     console.log(hero);//allu
+//     console.log(sidechar);//pk
+//     devara("ntr", "prakash")
+//     console.log(hero + sidechar);//allpk
+//     devara(hero,sidechar);//error ntr undefined allu pk
+
+// }
+// function devara(hero, villan) {
+//     console.log(hero + villan);//ntrprkash, undefined nan 
+
+// }
+// pushpa("allu", "PK")
+
+// function sum1(a,b,c){
+// console.log(a+b+sum2(a));//60
+// console.log(c);
+
+
+// }
+// function sum2(d){
+//     console.log(d+20);
+    
+// }
+// sum1(10, 20, 30)
+
+
+// function print(a,b,c){
+//     console.log(a+b+(c+a));
+//     console.log(c);
+//     printer(c+20)
+    
+    
+// }
+// function printer(d){
+//     console.log(d+34);
+    
+// }
+// print(34,43,5);
+
+// /**
+//  * 116 5 59
+//  * 116 5 25
+//  * 
+//  */
+
+
+
+
+// function print(){
+//     return "Hello There"
+// }
+// // var result=print();
+// // console.log(result);
+// console.log(print());
+
+// function sum(a, b, c){
+//     console.log(a+b+sub(c));
+//     return sub(a)
+    
+// }
+// function sub(a){
+//     console.log("hello");
+//     return a+20;
+    
+// }
+// sum(10,20, 30)
+
+//80
+
+// function print(){
+
+//     console.log(x);
+//     return "hello there"
+//     var x=10;
+    
+// }
+// print();
