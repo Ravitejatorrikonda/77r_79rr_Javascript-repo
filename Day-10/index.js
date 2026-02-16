24aacf6d461fba8cd383219d57b52498c4d16d82
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2026-02-16 16:53:15 +0530

    scopes

---
+++ Day-10/index.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ * 
+ *scopes in js:-
+ --------------
+ 
+{
+ 
+}
+
+func{
+
+}
+ * 
+ * 
+ */
+
+
+
+
+// const a=43;
+// // a=43;
+// const a;
+// console.log(a);
+
+// console.log(a);
+// let a=20;
+
+
+
