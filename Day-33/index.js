f58cf11706ca831c57a9098c9977b8e3c1d4f207
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2026-04-06 16:36:32 +0530

    Clousers

---
+++ Day-33/index.js
@@ -0,0 +1,81 @@
+/**
+ * history of js
+ * GEC
+ * FEC
+ * hoisting
+ * variables
+ * datatypes 
+ * operators
+ * conditional
+ * loops
+ * functions
+ * scopes
+ * synchronus and asynchronus
+ * callstack
+ * callback hell
+ * promises
+ * json
+ * fetch
+ * arrays method
+ * iterative mrethods
+ * async and await
+ * exception handkling
+ * DOM
+ * CRUD
+ * Events
+ * 
+ * 
+ * Lixical scope and clousers
+ * BOM
+ * 
+ * 
+ * lixical scope and clousers:-
+ * ----------------------------
+ * 
+ * 
+ */
+
+// function x() {
+//     let a = 10;
+//     function y() {
+//         console.log(a)
+//     }
+//     y();
+
+// }
+// x();
+
+
+// function x(){
+//     let a=10;
+//     return function y(){
+//         console.log(a)
+//     }
+// }
+// let a=x();
+// a();
+
+
+
+// for(let i=1; i<=5;i++){
+//     setTimeout(() => {
+//         console.log(i)
+//     }, 1000);
+// }
+
+
+// function addcount(intialcount){
+//     let count=intialcount;
+//     return (totalcount)=>{
+//         count +=totalcount;
+//         console.log(count)
+//     }
+
+// }
+// let cart=addcount(0);
+// cart(1);
+
+// let reg=/hello/;
+// console.log(reg.test("hello"))
+// let reg=new RegExp("Javascript")
+// console.log(reg.test("[6-9][0-9]{9}"))
